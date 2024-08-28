5f0040983f397c92ff2030234e92df2620480f0e
SofiAzeda <sofia811801@gmail.com>
2024-08-28 19:44:06 -0300

    Tutorial5

--- tutorial4/src/main.rs
+++ tutorial4/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Data Types");
 
-    let x: i32 = 2; //i32 integer, it's using 32 bits
+    let _x: i32 = 2; //i32 integer, it's using 32 bits
                     /*types
                     types of integer in bits
                     i8
@@ -10,9 +10,26 @@ fn main() {
                     i64
                     i128 */
 
-    let x: u32 = 247; //u means that can't hold numbers with signs ex: -35
+    let _x: u32 = 247; //u means that can't hold numbers with signs ex: -35
 
-    let flating_point: f32 = 10.5;
+    let _flating_point: f32 = 10.5;
 
-    let true_or_false: bool = 1;
+    let _true_or_false: bool = true;
+ 
+    let _letter: char = 'y';
+
+
+    //tupples
+
+    let mut tup: (i32, bool, i32, i32) = (1, true, -36, 35);
+
+    tup.0= 8;
+
+    println! ("{}", tup.0 );
+
+    // arrays
+
+    let _arr:[i32; 4] = [5, 3, 6, 3];
+
+    
 }
